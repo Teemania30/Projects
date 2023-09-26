c5cfb89b4156400c602c952abe4b8e3100d6af74
Tolu <teemania.29@gmail.com>
2023-09-26 14:58:44 +0100

    Added new things

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -5,4 +5,5 @@ disney = input("What Disney Princess is your favorite?:\n ")
 star_sign = input("What Star Sign are you?:\n ")
 
 print("Your Twitter Username is " + disney+ " " + star_sign )
-
+print("Your Twitter Username is " + disney+ " " + star_sign )
+print("Your Twitter Username is " + disney+ " " + star_sign )
