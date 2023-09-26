996a7fdd12a54fff64e4699f38c4b3e0cd611baf
Tolu <teemania.29@gmail.com>
2023-09-26 14:46:57 +0100

    First Commit

---
+++ bandnamegenerator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Twitter Name Generator!")
+
+disney = input("What Disney Princess is your favorite?:\n ")
+
+star_sign = input("What Star Sign are you?:\n ")
+
+print("Your Twitter Username is " + disney+ " " + star_sign )
+
